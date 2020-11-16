d6aba4f3d641ec8d3d083cd0f2410fcec4dc4570
Forellko <dragonforce97q@gmail.com>
2020-11-16 18:46:59 +0300

    Теперь добавляет элементы в список через template

--- index.js
+++ index.js
@@ -4,15 +4,8 @@ let inputForm = document.querySelector(".form-add__input");
 addedTask.addEventListener("submit", (evt) => {
   evt.preventDefault();
   let taskList = document.querySelector(".content__task-list");
-  let node = document.createElement("li");
-  let textNode = document.createTextNode(inputForm.value);
-
-  let templateNode = document.querySelector(".template__task");
-  let newTemplateNode = templateNode.content;
-  newTemplateNode.textContent = inputForm.value;
-
-  node.appendChild(textNode);
-  taskList.appendChild(node);
-  taskList.appendChild(templateNode.content);
+  let template = document.querySelector(".template__task").content.cloneNode(true);
+  template.querySelector("li").append(inputForm.value);
+  taskList.appendChild(template);
   inputForm.value = "";
 });
