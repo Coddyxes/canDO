bf2bc979245b98c99ee4e4c5271c59604df4d592
Alexandr <axkuz97@yandex.ru>
2020-11-17 14:21:08 +0300

    add delete button

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ addedTask.addEventListener("submit", (evt) => {
   evt.preventDefault();
   let taskList = document.querySelector(".content__task-list");
   let template = document.querySelector(".template__task").content.cloneNode(true);
+  let deleteButton = document.createElement("button");
+  let dltBtnText = document.createTextNode("Delete item");
+  deleteButton.appendChild(dltBtnText); 
+  deleteButton.onclick = function(){
+   this.parentElement.remove();
+  };
   template.querySelector("li").append(inputForm.value);
+  template.querySelector("li").append(deleteButton);
   taskList.appendChild(template);
   inputForm.value = "";
 });
