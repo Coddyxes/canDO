0f4e79bf256cdec20f53c2464c685af3f9ba99a0
Forellko <dragonforce97q@gmail.com>
2020-11-18 12:10:00 +0300

    Добавлена кнопка "Выполнено".

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const addedTask = document.querySelector(".content__form-add");
 const inputForm = document.querySelector(".form-add__input");
+let isCompleted = false;
 
 addedTask.addEventListener("submit", (evt) => {
   evt.preventDefault();
   const taskList = document.querySelector(".content__task-list");
   const template = document.querySelector(".template__task").content.cloneNode(true);
-  const deleteButton = template.querySelector("button");
-  const textAreaLi = template.querySelector("textarea");
+  const deleteButton = template.querySelector(".task__close");
+  const textAreaLi = template.querySelector(".task__text");
   textAreaLi.value = inputForm.value;
   deleteButton.onclick = function(){
-   this.parentElement.remove();
+   this.parentElement.parentElement.remove();
   };
+  const completeButton = template.querySelector(".task__complete");
+  completeButton.onclick = () => {
+    isCompleted = !isCompleted;
+    if(isCompleted){
+      textAreaLi.classList.add("task__text--complete");
+      completeButton.classList.add("task__complete--set");
+    } else {
+      textAreaLi.classList.remove("task__text--complete");
+      completeButton.classList.remove("task__complete--set");
+    }
+  }
   taskList.appendChild(template);
   inputForm.value = "";
 });
