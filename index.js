bd09e222e0a6da5a44a9040e7b8997ba1b326594
Forellko <dragonforce97q@gmail.com>
2020-11-18 10:18:52 +0300

    Кнопка закрытия теперь в template

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-let addedTask = document.querySelector(".content__form-add");
-let inputForm = document.querySelector(".form-add__input");
+const addedTask = document.querySelector(".content__form-add");
+const inputForm = document.querySelector(".form-add__input");
 
 addedTask.addEventListener("submit", (evt) => {
   evt.preventDefault();
-  let taskList = document.querySelector(".content__task-list");
-  let template = document.querySelector(".template__task").content.cloneNode(true);
-  let deleteButton = document.createElement("button");
-  let dltBtnText = document.createTextNode("Delete item");
-  deleteButton.appendChild(dltBtnText); 
+  const taskList = document.querySelector(".content__task-list");
+  const template = document.querySelector(".template__task").content.cloneNode(true);
+  const deleteButton = template.querySelector("button");
   deleteButton.onclick = function(){
    this.parentElement.remove();
   };
-  template.querySelector("li").append(inputForm.value);
-  template.querySelector("li").append(deleteButton);
+  template.querySelector("li").prepend(inputForm.value);
   taskList.appendChild(template);
   inputForm.value = "";
 });
